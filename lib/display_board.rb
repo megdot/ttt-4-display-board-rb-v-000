406bfc7f3f89c196bf083e0ccffe3f20d5630f6c
Megan Dotto <megan.dotto@gmail.com>
2017-12-06 20:15:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+puts "Welcome to Tic Tac Toe"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
