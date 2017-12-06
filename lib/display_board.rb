784e3938abe55f8745f75bb43332b28b9ea0b282
Megan Dotto <megan.dotto@gmail.com>
2017-12-06 21:04:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board(the_board) # what's made in a method stays in a method
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{the_board[0]} | #{the_board[1]} | #{the_board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{the_board[3]} | #{the_board[4]} | #{the_board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{the_board[6]} | #{the_board[7]} | #{the_board[8]} "
 end #Building a machine.
 
 #Main Universe
@@ -15,6 +15,3 @@ my_variable = "SHH DON'T TELL"
 display_board(my_variable)
 
 #display_board
-
-board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
-puts "Turn 2 happened, "
