5863248a685c54902347bfb74eabf28a63871242
Megan Dotto <megan.dotto@gmail.com>
2017-12-06 21:03:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-def display_board(the_board) # what's made in a method stays in a method 
+def display_board(the_board) # what's made in a method stays in a method
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
