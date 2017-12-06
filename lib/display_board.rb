bf77aec88a6e4322fed36506841740e1b5f651de
Megan Dotto <megan.dotto@gmail.com>
2017-12-06 21:02:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,17 +1,20 @@
-# Define display_board that accepts a board and prints
-# out the current state.
-def display_board
-  puts "   |   |   "
+def display_board(the_board) # what's made in a method stays in a method 
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end #Building a machine.
 
-puts "Welcome to Tic Tac Toe"
+#Main Universe
+# puts "Welcome to Tic Tac Toe"
 
-puts "Turn 1 Happened, X went in the middle"
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-display_board
+puts board[0]
+my_variable = "SHH DON'T TELL"
+display_board(my_variable)
 
-puts ""
+#display_board
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+puts "Turn 2 happened, "
